a424b612152bef1f9b6f86e6187e89c4f3b6d011
Patrick Reagan <reaganpr@gmail.com>
2012-09-05 17:32:08 -0600

    Finish exercise #8

--- Makefile
+++ Makefile
@@ -2,26 +2,5 @@ CFLAGS=-Wall -g
 
 all: ex1 ex3 ex4 ex5 ex6 ex7 ex8
 
-ex1:
-	cc $(CFLAGS) ex1.c -o ex1
-
-ex3:
-	cc $(CFLAGS) ex3.c -o ex3
-
-ex4:
-	cc $(CFLAGS) ex4.c -o ex4
-
-ex5:
-	cc $(CFLAGS) ex5.c -o ex5
-
-ex6:
-	cc $(CFLAGS) ex6.c -o ex6
-
-ex7:
-	cc $(CFLAGS) ex7.c -o ex7
-
-ex8:
-	cc $(CFLAGS) ex8.c -o ex8
-
 clean:
-	rm -rf ex{1,3,4,8} *.dSYM
+	rm -rf ex[1-9] *.dSYM
--- ex8.c
+++ ex8.c
@@ -5,10 +5,15 @@ int main(int argc, char *argv[]) {
   char name[]      = "Patrick";
   char full_name[] = {'P', 'a', 't', 'r', 'i', 'c', 'k', ' ', 'R', '.', ' ', 'R', 'e', 'a', 'g', 'a', 'n', '\0'};
 
+  areas[5] = 234;
+  areas[7] = 200;
+  name[0] = 'R';
+  areas[6] = full_name[2];
+
   printf("The size of an int: %ld\n", sizeof(int));
   printf("The size of areas (int[]): %ld\n", sizeof(areas));
   printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
-  printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
+  printf("The first area is %d, the 2nd %d, the 6th %d, the 7th %d, the 8th %d.\n", areas[0], areas[1], areas[5], areas[6], areas[7]);
 
   printf("The size of a char: %ld\n", sizeof(char));
   printf("The size of name (char[]): %ld\n", sizeof(name));
